6d5b8674d5ab92993ddf0d77160dff8c86352857
Stephen Sherratt <stephen@sherra.tt>
2024-06-06 09:09:08 +0200

    Updates

--- gridbugs-dev-ubuntu-osdev-cross-compile-to-x86/x.sh
+++ gridbugs-dev-ubuntu-osdev-cross-compile-to-x86/x.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -ex
+pushd ../gridbugs-dev-ubuntu && sh build && popd
 pushd ../gridbugs-dev-ubuntu-osdev && sh build && popd
 sh build
 docker run --rm -it gridbugs-dev-ubuntu-osdev-cross-compile-to-x86 bash --login
